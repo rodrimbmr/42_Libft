8fbd2b9fe89523971ad2017d81aa5963e3ae525b
Rodrigo <rodrigombmr@example.com>
2025-03-18 02:55:21 +0100

    added atol

--- ft_atol.c
+++ ft_atol.c
@@ -1,20 +1,32 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_isspace.c                                       :+:      :+:    :+:   */
+/*   ft_atol.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: rmonfort <rmonfort@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/03/18 01:36:03 by rmonfort          #+#    #+#             */
-/*   Updated: 2025/03/18 02:33:47 by rmonfort         ###   ########.fr       */
+/*   Created: 2025/03/18 02:00:20 by rmonfort          #+#    #+#             */
+/*   Updated: 2025/03/18 02:53:12 by rmonfort         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int	ft_isspace(int c)
+static long	ft_atol(const char *s)
 {
-	if (c == 32 || (c >= 9 && c <= 13))
-		return(1);
-	return(0);
+	long	result;
+	int		sign;
+
+	result = 0;
+	sign = 1;
+	while (ft_isspace(*s))
+		s++;
+	if (*s == '-' || *s == '+')
+	{
+		if(*s == '-')
+			sign = -1;
+	}
+	while (ft_isdigit(*s))
+		result = result *10 + (*s++ - '0');
+	return (result *sign);
 }
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: rmonfort <rmonfort@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/26 11:56:07 by rmonfort          #+#    #+#             */
-/*   Updated: 2025/03/18 01:44:14 by rmonfort         ###   ########.fr       */
+/*   Updated: 2025/03/18 02:54:10 by rmonfort         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,41 +16,42 @@
 # include <stdlib.h>
 # include <unistd.h>
 
-void	ft_bzero(void *s, size_t n);
-int		ft_isalnum(int c);
-int		ft_isalpha(int c);
-int		ft_isascii(int c);
-int		ft_isdigit(int c);
-int		ft_isprint(int c);
-void	*ft_memcpy(void *dest, const void *src, size_t n);
-void	*ft_memmove(void *dest, const void *src, size_t n);
-void	*ft_memset(void *s, int c, size_t n);
-size_t	ft_strlcat(char *dst, const char *src, size_t size);
-size_t	ft_strlcpy(char *dst, const char *src, size_t size);
-int		ft_toupper(int c);
-int		ft_tolower(int c);
-char	*ft_strchr(const char *s, int c);
-char	*ft_strrchr(const char *s, int c);
-int		ft_memcmp(const void *s1, const void *s2, size_t n);
-int		ft_strncmp(char *s1, char *s2, size_t n);
-void	*ft_memchr(const void *s, int c, size_t n);
-char	*ft_strnstr(const char *big, const char *little, size_t len);
-size_t	ft_strlen(const char *str);
-int		ft_atoi(const char *nptr);
-void	*ft_calloc(size_t nmemb, size_t size);
-char	*ft_strdup(const char *s);
-char	*ft_substr(char const *s, unsigned int start, size_t len);
-char	*ft_strjoin(char const *s1, char const *s2);
-char	*ft_strtrim(char const *s1, char const *set);
-char	**ft_split(char const *s, char c);
-char	*ft_itoa(int n);
-char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
-void	ft_striteri(char *s, void (*f)(unsigned int, char*));
-void	ft_putchar_fd(char c, int fd);
-void	ft_putstr_fd(char *s, int fd);
-void	ft_putnbr_fd(int n, int fd);
-void	ft_putendl_fd(char *s, int fd);
-int		ft_isspace(int c);
+void		ft_bzero(void *s, size_t n);
+int			ft_isalnum(int c);
+int			ft_isalpha(int c);
+int			ft_isascii(int c);
+int			ft_isdigit(int c);
+int			ft_isprint(int c);
+void		*ft_memcpy(void *dest, const void *src, size_t n);
+void		*ft_memmove(void *dest, const void *src, size_t n);
+void		*ft_memset(void *s, int c, size_t n);
+size_t		ft_strlcat(char *dst, const char *src, size_t size);
+size_t		ft_strlcpy(char *dst, const char *src, size_t size);
+int			ft_toupper(int c);
+int			ft_tolower(int c);
+char		*ft_strchr(const char *s, int c);
+char		*ft_strrchr(const char *s, int c);
+int			ft_memcmp(const void *s1, const void *s2, size_t n);
+int			ft_strncmp(char *s1, char *s2, size_t n);
+void		*ft_memchr(const void *s, int c, size_t n);
+char		*ft_strnstr(const char *big, const char *little, size_t len);
+size_t		ft_strlen(const char *str);
+int			ft_atoi(const char *nptr);
+void		*ft_calloc(size_t nmemb, size_t size);
+char		*ft_strdup(const char *s);
+char		*ft_substr(char const *s, unsigned int start, size_t len);
+char		*ft_strjoin(char const *s1, char const *s2);
+char		*ft_strtrim(char const *s1, char const *set);
+char		**ft_split(char const *s, char c);
+char		*ft_itoa(int n);
+char		*ft_strmapi(char const *s, char (*f)(unsigned int, char));
+void		ft_striteri(char *s, void (*f)(unsigned int, char*));
+void		ft_putchar_fd(char c, int fd);
+void		ft_putstr_fd(char *s, int fd);
+void		ft_putnbr_fd(int n, int fd);
+void		ft_putendl_fd(char *s, int fd);
+int			ft_isspace(int c);
+static long	ft_atol(const char *s);
 //BONUS
 
 typedef struct s_list
